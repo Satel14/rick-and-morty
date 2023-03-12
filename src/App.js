93439b8ba1b1e363d757bf6b4bf8b8747d1284fe
Satel14 <ostaplvov@gmail.com>
2023-03-12 15:03:53 +0200

    feat: added character list component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios'
 function App() {
+  const [data, setData] = useState({ result: [] })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="results">
+      <ul className='list'>
+        {data?.results && data.results.map((item) => (
+          <li className='item' key={item.id}>
+            <a href='/'>
+              <div className='item-img'>
+                <img src={item.image} alt={item.name} />
+              </div>
+            </a>
+            <div className='item-description'>
+              <h3 key={item.id} className="title">{item.name}</h3>
+              <h4 key={item.id} className="gender">{item.species}</h4>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
+
   );
 }
 
